17727b37c15e185916c77c9525e7b96f2ca4920b
Ronaldo Nunes <rndnunes@hotmail.com>
2017-10-09 21:05:50 -0300

    primeiros passo para enteder o node js

---
+++ Alura/nodejs/payfast/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.listen(3000, function(){
+    console.log('Servidor rodando na porta 3000.');
+});
+
+app.get('/teste', function(req, res){
+    console.log('Recebida requisicao de teste na porta 3000.');
+    res.send('ok');    
+});
+
